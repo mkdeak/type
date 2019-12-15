41783ac866cf9eb7076bcd43c278e4d922700f60
Matt Kovacs-Deak <matekd@users.noreply.github.com>
2019-12-15 22:11:53 +0000

    navbar dissappears if user clicks out

--- js/app.js
+++ js/app.js
@@ -1,23 +1,32 @@
 $(document).ready(function() {
-
-  'use strict';
+  "use strict";
 
   // =================
   // Responsive videos
   // =================
 
-  $('.o-wrapper').fitVids({
-    'customSelector': ['iframe[src*="ted.com"]']
+  $(".o-wrapper").fitVids({
+    customSelector: ['iframe[src*="ted.com"]']
   });
 
   // =================
   // Off Canvas menu
   // =================
 
-  $('.js-off-canvas-toggle').click(function(e) {
-    e.preventDefault();
-    $('.js-off-canvas-toggle').toggleClass('is-active');
-    $('.js-off-canvas-container').toggleClass('is-active');
-  });
+  $(".js-off-canvas-container").click(function(e) {
+    var $container = $(this);
+    var $target = $(e.target);
+    var $offcanvas = $(".c-off-canvas-content").first();
+    var $targetIsOnNavbar =
+      $.contains($offcanvas, $target) && $offcanvas == $target;
 
+    if ($target.hasClass("js-off-canvas-toggle")) {
+      e.preventDefault();
+      $(".js-off-canvas-toggle").toggleClass("is-active");
+      $(".js-off-canvas-container").toggleClass("is-active");
+    } else if ($container.hasClass("is-active") && !$targetIsOnNavbar) {
+      $(".js-off-canvas-toggle").toggleClass("is-active");
+      $(".js-off-canvas-container").toggleClass("is-active");
+    }
+  });
 });
